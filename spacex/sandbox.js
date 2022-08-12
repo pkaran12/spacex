07795748f5257c62e4ea8414d83563dccac2dae6
pkaran12 <108934219+pkaran12@users.noreply.github.com>
2022-08-12 15:37:11 +0530

    create spacex clone

---
+++ spacex/sandbox.js
@@ -0,0 +1,27 @@
+const btn = document.getElementById('menu-btn');
+const overlay = document.getElementById('overlay');
+const menu = document.getElementById('mobile-menu');
+
+let scrollStarted = false;
+
+btn.addEventListener('click', navToggle);
+document.addEventListener('scroll', scrollPage);
+
+function navToggle() {
+  btn.classList.toggle('open');
+  overlay.classList.toggle('overlay-show');
+  document.body.classList.toggle('stop-scrolling');
+  menu.classList.toggle('show-menu');
+}
+
+function scrollPage() {
+  const scrollPos = window.scrollY;
+
+  if (scrollPos > 100 && !scrollStarted) {
+    countUp();
+    scrollStarted = true;
+  } else if (scrollPos < 100 && scrollStarted) {
+    reset();
+    scrollStarted = false;
+  }
+}
